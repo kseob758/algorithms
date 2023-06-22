7565adac119fb606c796459e343ae3af537e3cc4
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-06-22 15:58:44 +0900

    달리기 경주.py

---
+++ Programmers/lv1/178871 달리기 경주/달리기 경주.py
@@ -0,0 +1,8 @@
+def solution(players, callings):
+    race = {j:i for i, j in enumerate(players)}  # 이름:등수
+    for call in callings:
+        rank = race[call]  # 등수
+        race[players[rank]] -= 1  # 딕셔너리의 등수 변경
+        race[players[rank-1]] += 1
+        players[rank], players[rank-1] = players[rank-1], players[rank]  # 순서 변경
+    return players
