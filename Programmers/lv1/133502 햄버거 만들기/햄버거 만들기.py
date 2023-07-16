a8ad0dd89d4690254ad57a926dc2d4b50ad3357e
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-07-16 23:36:35 +0900

    timeout 유의

--- Programmers/lv1/133502 햄버거 만들기/햄버거 만들기.py
+++ Programmers/lv1/133502 햄버거 만들기/햄버거 만들기.py
@@ -1,3 +1,4 @@
+# 슬라이싱이 pop or del 활용보다 느리다.
 def solution(ingredient):
     l = []
     answer = 0
@@ -5,5 +6,5 @@ def solution(ingredient):
         l.append(i)
         if l[-4:] == [1, 2, 3, 1]:
             answer += 1
-            del l[-4:]
+            del l[-4:]  # 이 부분 l = l[-4:] (슬라이싱 활용)하면 타임아웃
     return answer
