cd5d2369c0d17254fdc9543bb9d6daa7e564533e
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-07-19 21:28:35 +0900

    공원 산책.py

---
+++ Programmers/lv1/172928 공원 산책/공원 산책.py
@@ -0,0 +1,30 @@
+def solution(park, routes):
+    # 시작 위치(S) 찾기
+    for i in range(len(park)):
+        for j in range(len(park[0])):
+            if park[i][j] == 'S':
+                x, y = j, i
+                break
+    
+    for route in routes:
+        xx = x
+        yy = y
+        for step in range(int(route[2])):
+            if route[0] == 'E' and xx != len(park[0])-1 and park[yy][xx+1] != 'X':
+                xx += 1
+                if step == int(route[2])-1:
+                    x = xx
+            elif route[0] == 'W' and xx != 0 and park[yy][xx-1] != 'X':
+                xx -= 1
+                if step == int(route[2])-1:
+                    x = xx
+            elif route[0] == 'S' and yy != len(park)-1 and park[yy+1][xx] != 'X':
+                yy += 1
+                if step == int(route[2])-1:
+                    y = yy
+            elif route[0] == 'N' and yy != 0 and park[yy-1][xx] != 'X':
+                yy -= 1
+                if step == int(route[2])-1:
+                    y = yy
+
+    return [y, x]
