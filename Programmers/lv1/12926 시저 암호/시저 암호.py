cbb0938a727726655dd1c7eb04044557b16814b2
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-08-24 22:18:59 +0900

    시저 암호.py

--- Programmers/lv1/12926 시저 암호/시저 암호.py
+++ Programmers/lv1/12926 시저 암호/시저 암호.py
@@ -1,19 +1,19 @@
 def solution(s, n):
-    lst = list(s)
+    lst = list(s) # 문자 하나씩 들어간 리스트
     answer = ''
     for a in lst:
-        if a == ' ':
+        if a == ' ': # 문자가 공백이면 처리 없이 추가
             answer += a
         
-        elif 65 <= ord(a) <= 90:
-            new = ord(a)+n
-            if new > 90:
-                new -= 26
-            answer += chr(new)
-        elif 97 <= ord(a) <= 122:
+        elif 65 <= ord(a) <= 90: # 소문자면
             new = ord(a)+n # 밀기
-            if new > 122:
-                new -= 26
+            if new > 90: # 소문자 코드 범위를 넘어가면
+                new -= 26 # 순환으로 만들기
+            answer += chr(new)
+        elif 97 <= ord(a) <= 122: # 대문자면
+            new = ord(a)+n # 밀기
+            if new > 122: # 대문자 코드 범위를 넘어가면
+                new -= 26 # 순환으로 만들기
             answer += chr(new)
     
     return answer
