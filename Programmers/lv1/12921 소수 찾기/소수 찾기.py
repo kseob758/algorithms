6b49c6b1c46413ba6465400d2a285537ed26956f
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-08-27 21:46:28 +0900

    소수찾기 add comment

--- Programmers/lv1/12921 소수 찾기/소수 찾기.py
+++ Programmers/lv1/12921 소수 찾기/소수 찾기.py
@@ -1,9 +1,9 @@
 def solution(n):
-    prime = [1] * (n+1)
+    prime = [1] * (n+1)  # index 0~n
     
-    for i in range(2, n+1):
-        if prime[i] == 1:
-            for j in range(i*2, n+1, i):
-                prime[j] = 0
+    for i in range(2, n+1):  # 2부터 n까지
+        if prime[i] == 1:  # prime[i]가 1이면 (소수이면)
+            for j in range(i*2, n+1, i):  # i의 배수인 i*2부터 i*3, i*4, ... <=n 까지
+                prime[j] = 0  # 소수가 아닌 것으로 판명
                 
-    return prime.count(1)-2
+    return prime.count(1)-2  #초기화 할 때 들어간 index 0,1 빼주기
