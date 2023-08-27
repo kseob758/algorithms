89309461b8c9684e906d2d049ce56e3ff96059dd
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-08-27 21:44:48 +0900

    소수 찾기.py

---
+++ Programmers/lv1/12921 소수 찾기/소수 찾기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    prime = [1] * (n+1)
+    
+    for i in range(2, n+1):
+        if prime[i] == 1:
+            for j in range(i*2, n+1, i):
+                prime[j] = 0
+                
+                
+    return prime.count(1)-2
