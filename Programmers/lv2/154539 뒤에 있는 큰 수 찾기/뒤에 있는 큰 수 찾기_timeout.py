6604c5d895281d0d1d80b77ef48bb4b57813a246
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-09-05 22:58:58 +0900

    뒤에 있는 큰 수 찾기_timeout.py

---
+++ Programmers/lv2/154539 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기_timeout.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer = []
+    for i, num in enumerate(numbers):
+        dcs = -1
+        for j in range(i, len(numbers)):
+            if numbers[j] > num:
+                dcs = numbers[j]
+                break
+        answer.append(dcs)
+        
+    return answer
