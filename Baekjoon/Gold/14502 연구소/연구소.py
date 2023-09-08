0f57b8204bad287f0bb1a7ccbdacedda3a114a73
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-09-08 21:43:56 +0900

    Add comments to 연구소.py

--- Baekjoon/Gold/14502 연구소/연구소.py
+++ Baekjoon/Gold/14502 연구소/연구소.py
@@ -13,12 +13,14 @@ def calculate_area_size():
     while q:
         x, y = q.popleft()
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+            nx, ny = x + dx[i], y + dy[i]  # 상하좌우
+            # 맵을 벗어나지 않으면서 벽 설치가 가능해야함
             if nx < 0 or nx >= n or ny < 0 or ny >= m or check_map[nx][ny] != 0:
                 continue
             check_map[nx][ny] = 2  # virus spread
             q.append((nx, ny))
 
+    # 안전 영역(0의 개수) 확인
     global answer
     cnt = 0
     for c in check_map:
@@ -41,10 +43,13 @@ for x in range(n):
             virus.append((x, y))
 
 answer = 0
-for blank in itertools.combinations(blank, 3):
+for blank in itertools.combinations(blank, 3):  # 설치 가능한 모든 조합
+    # 벽 3개 설치
     for wx, wy in blank:
         lab_map[wx][wy] = 1
+    # 안전 영역 크기 확인
     calculate_area_size()
+    # 벽 3개 제거
     for wx, wy in blank:
         lab_map[wx][wy] = 0
         
