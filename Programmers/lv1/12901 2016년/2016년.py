2ff5676ed0ffab9f06cd14d47ab00eb1fc53b9f3
kseob758 <125840318+kseob758@users.noreply.github.com>
2023-09-09 23:00:51 +0900

    Add comment to 2016년.py

--- Programmers/lv1/12901 2016년/2016년.py
+++ Programmers/lv1/12901 2016년/2016년.py
@@ -1,10 +1,10 @@
 def solution(a, b):
-    total = 0
-    mon = [0,31,29,31,30,31,30,31,31,30,31,30]
-    day = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+    total = 0 # 총 일수
+    mon = [0,31,29,31,30,31,30,31,31,30,31,30]  # 1~12월까지의 월별 일수
+    day = ['THU','FRI','SAT','SUN','MON','TUE','WED']  # 1일 FRI로 하기 위한 순서
     
-    for i in range(a):
+    for i in range(a):  # a-1월까지의 일수 더하기
         total += mon[i]
-    total += b
+    total += b  # b일 더하기
     
     return day[((total%7))] 
